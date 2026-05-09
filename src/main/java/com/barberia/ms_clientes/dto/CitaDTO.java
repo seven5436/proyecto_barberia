a7a4fae28c82a5b9419ef2949ce63c20a35c48be
Leanplzx <l.matias.a.vergara@gmail.com>
2026-05-09 19:22:57 -0400

    corrección DTO cita

--- src/main/java/com/barberia/ms_clientes/dto/CitaDTO.java
+++ src/main/java/com/barberia/ms_clientes/dto/CitaDTO.java
@@ -3,33 +3,19 @@ package com.barberia.ms_clientes.dto;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
-@Table(name = "citas")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CitaDTO {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long idCita;
 
-    @ManyToOne
-    @JoinColumn(name = "id_barbero")
-    private Barbero barbero;
-
-    @ManyToOne
-    @JoinColumn(name = "id_cliente")
-    private Cliente cliente;
+    private String nombreBarbero;
+    private String nombreCliente;
 
     private LocalDate fechaCita;
     private LocalTime horaInicio;
